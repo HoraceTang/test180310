8a3fde56a1d5a48b2875de8a9ec11dfbdd0ecffc
HoraceTang <935453776@qq.com>
2018-03-10 23:16:10 +0800

    first commit : init project

---
+++ test180310/test180310/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test180310
+//
+//  Created by tanghong on 2018/3/10.
+//  Copyright © 2018年 tanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test180310/test180310/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test180310
+//
+//  Created by tanghong on 2018/3/10.
+//  Copyright © 2018年 tanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
